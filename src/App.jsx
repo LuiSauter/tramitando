24d14baefceb3f7d8d8ee1463b5f8c06fa140d4c
Luis Gabriel Janco <janco7249@gmail.com>
2023-11-23 18:28:59 -0400

    feat: setup run app

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Switch, Redirect, Route } from 'wouter'
+import HomePage from './pages/home'
+import DashboardPage from './pages/dashboard'
+import RegisterPage from './pages/register'
+import LoginPage from './pages/login'
+import Layout from './components/layout/index'
+
+import { AuthProvider } from './context/authContext'
+import { DescriptionTramitProvider } from './context/descriptionTramitContext'
+import { TramitProvider } from './context/tramitContext'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <AuthProvider>
+      <DescriptionTramitProvider>
+        <TramitProvider>
+          <Layout>
+            <Switch>
+              <Route path='/' component={<HomePage />} />
+              <Route path='/:id' component={<HomePage />} />
+              <Route path='/dashboard' component={<DashboardPage />} />
+              <Route path='/register' component={<RegisterPage />} />
+              <Route path='/login' component={<LoginPage />} />
+              <Route path='/:rest*'>
+                <Redirect to='/' />
+              </Route>
+            </Switch>
+          </Layout>
+        </TramitProvider>
+      </DescriptionTramitProvider>
+    </AuthProvider>
   )
 }
 
--- src/utils/contants.js
+++ src/utils/contants.js
@@ -1,1 +1,2 @@
 // crear constantes
+export const API_URL = 'https://tramitando-db.vercel.app/api'
